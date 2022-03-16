1509e742e186a0b12cd966c6d5d3b9f0b5c00130
Administrator <wuzhibin05@163.com>
2022-03-16 22:41:54 +0800

    add

--- src/go_code/chapter03/test/main/main.go
+++ src/go_code/chapter03/test/main/main.go
@@ -1,9 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
-	var a int = 300
-	var ptr *int = &a // 错误，变量类型不匹配
-	fmt.Println(ptr)
+	// string 转换为bool
+	var str1 string = "true"
+	var b bool
+	b, _ = strconv.ParseBool(str1)
+	fmt.Printf("b type is %T，b value is %v\n", b, b)
+	// string 转换为 int
+	var str2 string = "1023"
+	var num2 int64
+	var num3 int
+	num2, _ = strconv.ParseInt(str2, 10, 64)
+	num3 = int(num2)
+	fmt.Printf("num3 type is %T，num3 value is %v\n", num3, num3)
+	// string 转换浮点型
+	var str3 string = "23.43"
+	var c float64
+	c, _ = strconv.ParseFloat(str3, 64)
+	fmt.Printf("c type is %T，c value is %v\n", c, c)
 }
